7462bcefbef5e5ad3346daaca2313ce847baf5e7
adigreiman1 <adigreiman@gmail.com>
2024-11-11 19:54:17 +0200

    Update TimeFormat.java

--- TimeFormat.java
+++ TimeFormat.java
@@ -2,35 +2,31 @@ public class TimeFormat {
     public static void main(String[] args) {
       
         
-        int hours = Integer.parseInt(args[0]);
-        int minutes = Integer.parseInt(args[1]); 
-
-        int newHour;
-        if (hours == 0) { 
-            newHour = 0;
-        } else if (hours > 12) { 
-            newHour = hours - 12;
-        } else {
-            newHour = hours;
-        }
-
+        int time = Integer.parseInt(args[0]);
         
-        String minuteStr;
-        if (minutes < 10) {
-            minuteStr = "0" + minutes;
-        }
-        else {
-            minuteStr = ""+minutes;
-        }
-
-        String kindOfTime ;
-
-        if (hours < 12) {
-            kindOfTime = "AM";
-        } else {
-            kindOfTime = "PM";
-        }
+        int hours = Integer.parseInt(""+args[0].charAt(0) +args[0].charAt(1) );
+        int minuts = Integer.parseInt(""+args[0].charAt(3) +args[0].charAt(4) );
        
-        System.out.println(newHour + ":" + minuteStr + " " + kindOfTime);
+        if (hours > 12 ) { 
+       hours = hours -12;
+       
+
+        if ( minuts>= 10) { 
+       System.out.println(hours + ":0"+ minuts + "PM");
+        }
+
+        else {
+            System.out.println(hours + ":" + minuts+ "PM");
+        }
+    }
+    else{
+        if ( minuts>= 10) { 
+            System.out.println(hours + ":0"+ minuts + "AM");
+             }
+     
+             else {
+                 System.out.println(hours + ":" + minuts+ "AM");
+             }
     }
 }
+       
