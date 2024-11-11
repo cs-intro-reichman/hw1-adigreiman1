53aa8cd4e90d36c28126ee4979341de9b5a2bdd3
adigreiman1 <adigreiman@gmail.com>
2024-11-11 19:03:15 +0200

    Update NumWords.java

--- NumWords.java
+++ NumWords.java
@@ -8,6 +8,6 @@ public class NumWords {
     int hundreds =num /100;
 
 
-     System.out.println( hundreds +" hundreds, " + tens +" tens and " + ones+ " ones.");
+     System.out.println( hundreds +" hundreds, " + tens +" tens, and " + ones+ " ones.");
 }
 }
